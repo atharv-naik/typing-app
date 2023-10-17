7e2e156a403e843e18f9e080248bb6da49eb6458
atharv-naik <atharvn317@gmail.com>
2023-10-17 23:55:12 +0530

    update wpm canvas styles & show desktop-only page when accessed via mobile devices

--- index.js
+++ index.js
@@ -97,16 +97,21 @@ textArray = ["The quick brown fox jumps over the lazy dog"];
 function applyRandomColor() {
   randomLightColor = Math.floor(Math.random() * 360);
   // avoid very light colors
-  if (randomLightColor > 60 && randomLightColor < 120) {
-    randomLightColor += 60;
+  if (randomLightColor > 30 && randomLightColor < 200) {
+    randomLightColor = (randomLightColor + 170) % 360;
   }
-  document.body.style.backgroundColor = `hsl(${randomLightColor}, 100%, 80%)`;
+  console.log(randomLightColor);
+  // document.body.style.backgroundColor = `hsl(${randomLightColor}, 100%, 80%)`;
 
   // apply a corresponding dark shade on current-letter
   randomDarkColor = Math.floor(Math.random() * 360) + 180;
   document.body.style.setProperty(
     "--current-letter-bg-color",
     `hsl(${randomDarkColor}, 100%, 30%)`
+  );
+  document.body.style.setProperty(
+    "--body-bg-color",
+    `hsl(${randomLightColor}, 100%, 80%)`
   );
   document.body.style.setProperty(
     "--light-color",
@@ -169,6 +174,12 @@ const wpmGraphContext = wpmGraphCanvas.getContext("2d");
 function drawGraph(dataPoints) {
   wpmGraphContext.clearRect(0, 0, wpmGraphCanvas.width, wpmGraphCanvas.height); // Clear the canvas
   wpmGraphContext.beginPath();
+  wpmGraphContext.lineTo(
+    (dataPoints[0].x / dataPoints[dataPoints.length - 1].x) *
+      wpmGraphCanvas.width,
+    wpmGraphCanvas.height
+  );
+  // wpmGraphContext.lineTo(0, wpmGraphCanvas.height);
   dataPoints.forEach((dataPoint) => {
     wpmGraphContext.lineTo(
       (dataPoint.x / dataPoints[dataPoints.length - 1].x) *
@@ -182,6 +193,17 @@ function drawGraph(dataPoints) {
   wpmGraphContext.strokeStyle = "black";
   wpmGraphContext.lineWidth = 1;
   wpmGraphContext.stroke();
+  wpmGraphContext.fillStyle = "rgba(0, 0, 0, 0.2)";
+  wpmGraphContext.font = "12px monospace";
+  wpmGraphContext.fillText(
+    "time (s)",
+    wpmGraphCanvas.width - 60,
+    wpmGraphCanvas.height - 10
+  );
+  wpmGraphContext.fillText("WPM", 10, 10);
+  wpmGraphContext.lineTo(wpmGraphCanvas.width, wpmGraphCanvas.height);
+  wpmGraphContext.fill();
+  // wpmGraphContext.closePath();
 }
 
 // EVENT LISTENERS //
@@ -198,7 +220,6 @@ document.addEventListener("keydown", function (event) {
       level++;
       i = 0;
       newText();
-      applyRandomColor();
     }
     text.children[i].classList.add("current-letter");
   } else if (
@@ -227,11 +248,48 @@ fetch("corpus_cleaned.txt")
     );
   });
 
-level = 1;
-WPM = 0;
-i = 0;
-startTime = 0;
-applyRandomColor();
-newText();
-// update wpm every 200ms
-setInterval(updateWPM, 200);
+// If accessed from mobile, show a message
+window.mobileCheck = function () {
+  let check = false;
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4)
+      )
+    )
+      check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
+};
+
+if (mobileCheck()) {
+  // add a message
+  let messageWrapper = document.createElement("div");
+  messageWrapper.classList.add("desktop-only-wrapper");
+  let messageBg = document.createElement("img");
+  messageBg.src =
+    "https://www.gstatic.com/youtube/src/web/htdocs/img/monkey.png";
+  messageWrapper.appendChild(messageBg);
+  let message = document.createElement("div");
+  message.classList.add("desktop-only-message");
+  message.innerHTML =
+    "<span class='app-name__gradiant'>Speed-Type</span> is only available on desktop browsers";
+  messageWrapper.appendChild(message);
+  document.body.appendChild(messageWrapper);
+} else {
+  // INITIALIZE //
+  const container = document.querySelector(".container");
+  container.style.display = "block";
+
+  level = 1;
+  WPM = 0;
+  i = 0;
+  startTime = 0;
+  applyRandomColor();
+  newText();
+  // update wpm every 500ms
+  setInterval(updateWPM, 500);
+}
